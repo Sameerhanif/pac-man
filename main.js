9d053a936ce5d554f40fbbb33cb82681b10e6697
Sameerhanif <112457325+Sameerhanif@users.noreply.github.com>
2024-08-27 12:50:55 +0200

    ghosts gemaakt
    de ghosts hebben een lichaam dat te zien is in de grid.

--- main.js
+++ main.js
@@ -142,11 +142,29 @@ squares[pacmanCurrentIndex].classList.remove("power-pellet")
     }
 
     function unScaredGhosts() {
-
+ghosts.forEach(ghost => ghost.isScared = false)
     }
 
+    class Ghost {
+        constructor(className, startIndex, speed) {
+            this.className = className
+            this.startIndex = startIndex 
+            this.speed = speed 
+            this.currentIndex = startIndex 
+            this.isScared = false 
+            this.timerId = NaN
+        }
+    }
 
+const ghosts = [
+new Ghost("blinky",348,250),
+new Ghost("pinky", 367, 400),
+new Ghost("inky", 351, 300),
+new Ghost("clyde", 379, 500),
+]
 
+ghosts.forEach(ghost =>
+    squares[ghost.currentIndex].classList.add(ghost.className, "ghost"))
 
 
 })
